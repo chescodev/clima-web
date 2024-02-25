5d49319c2a3da8951a5e0a54a5f704d3ad3f62f0
Franchesco007 <franchesco1510@gmail.com>
2024-02-24 20:34:08 -0500

    Bento y Header

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+import midudev from '@midudev/tailwind-animations'
+import animations from 'tailwindcss-animated'
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [midudev, animations],
+}
